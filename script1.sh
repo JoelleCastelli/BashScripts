c0858a38f23dd3b9e97829d99d446ca42d77698b
Joëlle <castelli.jo@gmail.com>
2020-06-30 12:03:37 +0200

    fix + nettoyage + documentation script 1

--- script1.sh
+++ script1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# On déclare les constantes
 declare -r ROOT_UID=0
 declare -r SUCCESS=0
 declare -r ERROR=13
+declare -r NB_USERS=10
 
 # On vérifie que l'utilisateur soit root
 if [ $UID -ne $ROOT_UID ]
@@ -12,46 +14,41 @@ then
 fi  
 
 i=1
-while [ $i -le 10 ]
-	do
-		username=user$RANDOM
-		password=$username
+while [ $i -le $NB_USERS ]
+do
+	# On génère un nom aléatoire qui sera également le mot de passe
+	username=user$RANDOM
+	password=$username
 
-		# On vérifie si l'utilisateur existe (q pour quiet : on n'affiche rien)
-		grep -q "$username" /etc/passwd
+	# On vérifie si l'utilisateur existe déjà
+	grep -q "$username" /etc/passwd
+	if [ $? -eq $SUCCESS ] 
+	then
+		# Si oui : on recommence le processus sans incrémenter le compteur
+		echo "$i - L'utilisateur $username existe déjà : génération d'un nouveau nom"
+	else
+		# Si non : on crée un utilisateur avec un répertoire personnel et sans mot de passe
+		useradd -m -g users -s /bin/bash "$username"
 		if [ $? -eq $SUCCESS ] 
-		then	
-			echo "$i - L'utilisateur $username existe déjà : génération d'un nouveau nom"
-		else
-			# On crée un utilisateur sans mot de passe
-			# -m : crée un répertoire personnel
-			# -g users : met dans le groupe users
-			# -s /bin/bash : spécifie le shell bash par défaut pour le user
-			useradd -m -g users -s /bin/bash "$username"
-			if [ $? -eq $SUCCESS ] 
-				then
-					echo "$i - Le compte de $username est créé !"
-				else
-					echo "Aïe, problème dans la création de l'utilisateur $username :("
-			fi
+			then
+				echo -e "\n$i - Le compte de $username est créé !"
 
-			# On set un mot de passe (-e interprète les caractères échapés)
-			# On renvoie le prompt vers /dev/null pour ne pas polluer la console
-			echo -e "$password\n$password" | passwd $username >& /dev/null
+				# On set un mot de passe en renvoyant le prompt vers /dev/null pour ne pas polluer la console
+				echo -e "$password\n$password" | passwd $username >& /dev/null
 
-			# On crée les fichiers
-			nb_files=$((RANDOM%6+5))
-			echo "On va créer $nb_files fichiers..."
-			
-			for j in `seq 1 $nb_files`
+				# On génère aléatoirement le nombre de fichiers à créer
+				nb_files=$((RANDOM%6+5))
+				echo "On va créer $nb_files fichiers..."
+				
+				# Pour chaque fichier à créer
+				for j in `seq 1 $nb_files`
 				do
+					# On définit le nom, l'utilité de taille et une taille aléatoire
 					file_name=file$j
 					size_unit=M
 					file_size=$((RANDOM%46+5))
 
-					# On copie de /dev/random vers la cible et on écrit un bloc de la taille random
-					# On renvoie vers /dev/null pour ne pas polluer la console
-					#dd if=/dev/urandom of=/home/$username/$file_name bs=$file_size$size_unit count=1 >& /dev/null
+					# On alloue la mémoire nécessaire au fichier
 					fallocate -l $file_size$size_unit /home/$username/$file_name
 					if [ $? -eq $SUCCESS ] 
 					then
@@ -60,10 +57,13 @@ while [ $i -le 10 ]
 						echo "Aïe, problème dans la création du fichier $file_name :("
 					fi
 				done
-			echo " "
+			else
+				echo -e "\nAïe, problème dans la création de l'utilisateur $username :("
+		fi
 
-			# On passe à la création du prochain utilisateur
-			i=$(($i + 1))
-		fi  
-	done
-exit 0
+		# On incrémente le compteur pour créer l'utilisateur suivant
+		i=$(($i + 1))
+	fi  
+done
+
+exit $SUCCESS
